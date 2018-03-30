05a5328d51aabf8d1d2cda38fd9e7dbc33d8b4a9
lim-roid <limroid0408@163.com>
2018-03-30 10:34:39 +0800

    bug

---
+++ src/common/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <p>下拉选项的边框及选中状态可测试主题色</p>
+    <el-progress type="circle" :percentage="25"></el-progress>
+    <el-select v-model="value" placeholder="请选择">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value">
+    </el-option>
+  </el-select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value: ''
+    }
+  },
+  created () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
